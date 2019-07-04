4b5a9c2c5660a8dce955d80395d7f85e7cd7ffd6
Napat Sornprasit <nookmonsta@gmail.com>
2019-07-04 09:54:32 +0700

    eiei

--- Screen/FeedScreen.js
+++ Screen/FeedScreen.js
@@ -1,34 +1,37 @@
-import React, { Component } from 'react'
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
-import firebase, { firestore } from './firebase'
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, Image } from 'react-native';
+import firebase, { firestore } from './firebase';
+import Message from "../Message/Message";
+
+import { KeyboardAvoidingView } from "react-native";
 
 
 const Postin = (props) => {
-    let post = props.post
+    let post = props.post;
     return (
         <View style={styles.postBut}>
             <View>
-                <Text key={props.i} style = {styles.textTitle1}>
+                <Text key={props.i} style={styles.textTitle1}>
                     {post.user.name} :
                 </Text>
-                <Text style={{marginLeft: 20}} >
+                <Text style={{ marginLeft: 20 }} >
                     {post.text}
                 </Text>
             </View>
-            
+
         </View>
-    )
-}
+    );
+};
 
 export default class FeedScreen extends Component {
 
     constructor(props) {
-        super(props)
+        super(props);
 
         this.state = {
             post: "",
             myPosto: []
-        }
+        };
 
     }
 
@@ -58,18 +61,18 @@ export default class FeedScreen extends Component {
                         let post = doc.data().title.post
                         posts = [post, ...posts]
                     }
-                })
+                });
                 this.setState({
                     myPosto: posts
-                })
+                });
 
-            })
+            });
 
     }
 
     onPost = () => {
         let { post, myPosto } = this.state;
-        let { name } = this.props.navigation.state.params
+        let { name } = this.props.navigation.state.params;
 
         let newPost = {
             title: {
@@ -80,74 +83,86 @@ export default class FeedScreen extends Component {
                     }
                 }
             }
-        }
+        };
 
-        firestore.collection("feeds").doc('bangkok').collection('BangKhen').add(newPost)
+        firestore.collection("feeds").doc('bangkok').collection('BangKhen').add(newPost);
 
         this.setState({
             myPosto: [newPost.title.post, ...myPosto],
             post: ""
-        })
+        });
 
-    }
+    };
 
     render() {
         let myPosto = this.state.myPosto
 
         return (
-            <View style={styles.container}>
-
-                <View style={{ flex: 80, marginTop: 20 }}>
-
-                    <View style={{ marginLeft: 20, marginTop: 5 }}>
-                        <Text style={styles.textTitle} >Festival</Text>
-                    </View>
-                    <View style={{ marginLeft: 20 }}>
-                        <Text style={styles.subTitle}>in Bang Khen</Text>
-                    </View>
-                    <ScrollView>
-                        <View>
-                            {
-                                myPosto.map((post, index) => {
-                                    if (post) {
-                                        console.log("have");
-
-                                    } else {
-                                        console.log("not have");
-
-                                    }
-
-                                    return (<Postin post={post} key={index} i={index} />)
-                                })
-                            }
-                        </View>
-                    </ScrollView>
-                </View>
-                <View style={"height: 80%"}>
-                    <View style={styles.postbg}>
-                        <TextInput
-                            style={styles.PostInput}
-                            onChangeText={(text) => this.setState({ post: text })}
-                            value={this.state.post}
-                        />
-                        <View style={{ alignSelf: 'flex-end' }}>
-                            <View style={styles.butrealPost}>
-                                <Button style={{ justifyContent: 'flex-end' }}
-                                    title="Post"
-                                    onPress={this.onPost}
-                                    value={this.post}
-                                    color="#000000"
+            
+                <KeyboardAvoidingView
+                    style={styles.container}
+                    bahavior="padding"
+                    enabled
+                >
+                    <View style={styles.container}>
+                        <View style={{ flex: 80, marginTop: 20 }}>
+                            <View style={{ marginLeft: 20, marginTop: 5 }}>
+                                <Image
+                                    style={{ width: 50, height: 50 }}
+                                    source={require('./back.png')}
                                 />
+                                <Text style={styles.textTitle} >Festival</Text>
+                            </View>
+                            <View style={{ marginLeft: 20 }}>
+                                <Text style={styles.subTitle}>in Bang Khen</Text>
+                            </View>
+                            <ScrollView>
+                                <View>
+                                    {
+                                        myPosto.map((post, index) => {
+                                            if (post) {
+                                                console.log("have");
+
+                                            } else {
+                                                console.log("not have");
+
+                                            }
+
+                                            return (<Postin post={post} key={index} i={index} />);
+                                        })
+                                    }
+                                </View>
+                            </ScrollView>
+                        </View>
+                        <View>
+                            <View style={styles.postbg}>
+                                <TextInput
+                                    style={styles.PostInput}
+                                    onChangeText={(text) => this.setState({ post: text })}
+                                    value={this.state.post}
+                                />
+                                <View style={{ alignSelf: 'flex-end' }}>
+                                    <View style={styles.butrealPost}>
+                                        <Button style={{ fontSize: 14, justifyContent: 'center' }}
+                                            title="Post"
+                                            onPress={this.onPost}
+                                            value={this.post}
+                                            color="#000000"
+                                        />
+                                    </View>
+                                </View>
                             </View>
                         </View>
-                    </View>
-                </View>
-            </View >
+                    </View >
+                </KeyboardAvoidingView>
+            
 
 
-        )
+        );
     }
 }
+
+
 
 const styles = StyleSheet.create({
     container: {
@@ -162,8 +177,8 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         margin: 12,
         marginTop: 20,
-        height: 100,
-        width: 350,
+        height: 30,
+        width: 300,
         borderColor: 'gray',
         borderWidth: 1,
         backgroundColor: '#ffffff'
@@ -177,7 +192,7 @@ const styles = StyleSheet.create({
     textTitle1: {
         fontSize: 16,
         fontWeight: 'bold',
-        marginLeft: 10 
+        marginLeft: 10
 
 
     },
@@ -203,15 +218,19 @@ const styles = StyleSheet.create({
     },
     postbg: {
         backgroundColor: '#b0c4de',
+        height: 80,
+        display: 'flex',
+        flexDirection: 'row',
     },
     butrealPost: {
         borderRadius: 10,
         borderWidth: 1,
-        margin: 20,
-        height: 40,
-        width: 80,
+        marginBottom: 30,
+        height: 30,
+        width: 50,
         backgroundColor: '#add8e6',
         borderColor: '#ffffff',
+        fontSize: 14
     },
     textPost: {
         marginLeft: 20,
